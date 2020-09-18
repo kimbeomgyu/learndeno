09babb29ac334c9906452ef22a8b8ac1e770f97d
김범규 <beomgyu.kim@jcurve.xyz>
2020-09-18 22:24:00 +0900

    TEST

--- deps.ts
+++ deps.ts
@@ -1,3 +1,4 @@
 export { Application, Router } from "https://deno.land/x/oak/mod.ts";
 export { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import * as axiosd from "https://deno.land/x/axiod/mod.ts";
+import axiosd from "https://deno.land/x/axiod/mod.ts";
+export { axiosd };
