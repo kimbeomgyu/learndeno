53931e4d8ccd1f922cc9dfe488bf4d9c11cdbe27
김범규 <beomgyu.kim@jcurve.xyz>
2020-09-18 21:52:36 +0900

    set import * as

--- deps.ts
+++ deps.ts
@@ -1,5 +1,3 @@
 export { Application, Router } from "https://deno.land/x/oak/mod.ts";
 export { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import axiosd from "https://deno.land/x/axiod/mod.ts";
-
-export { axiosd };
+import * as axiosd from "https://deno.land/x/axiod/mod.ts";
